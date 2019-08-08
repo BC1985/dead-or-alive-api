b256d5b52b1c1846f40905ca65cc715ba776f5f2
Ben Cohen <benjcohen1985@gmail.com>
2019-08-08 15:58:36 -0400

    Add data to table

--- src/Seed/seed-table.sql
+++ src/Seed/seed-table.sql
@@ -1,1 +1,5 @@
-INSERT INTO people(person_name,dates,status,description) VALUES('Ludwig Van Beethoven', '1770-1827','Dead', 'Expired and joined the choir invisible');
+INSERT INTO people(person_name,dates,status,description) 
+VALUES ('George Carlin', 'May 12, 1937-June 22, 2008','Dead', 'Got his five minute warning'),
+('Zubin Mehta', 'Born April 29, 1936', 'Alive','Alive and smelling the roses'),
+('Andrew "Dice" Clay', 'Born September 29, 1957', 'Alive','Still among the living');
+('Ludwig Van Beethoven', '1770-1827','Dead', 'Expired and joined the choir invisible'),
