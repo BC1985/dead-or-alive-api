66e4e9fe5563c176de65393a75a03d99194c0973
Ben Cohen <benjcohen1985@gmail.com>
2019-08-08 16:09:02 -0400

    Add post person function

--- src/People/peopleServices.js
+++ src/People/peopleServices.js
@@ -1,6 +1,12 @@
 const peopleServices = {
   getPeople(knex) {
     return knex.select("*").from("people");
+  },
+  postPerson(knex, person) {
+    return knex
+      .returning(["id", "person_name", "dates", "status", "description"])
+      .from("people")
+      .insert(person);
   }
 };
 
