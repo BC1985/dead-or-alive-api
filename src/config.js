4d50534f0d14fd533daa5daa50aa34b3a8f12058
Ben Cohen <benjcohen1985@gmail.com>
2019-08-08 16:56:47 -0400

    Add test db URL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: "postgresql://postgres@localhost/dead-or-alive"
+  DB_URL: "postgresql://postgres@localhost/dead-or-alive",
+  TEST_DB_URL: "postgresql://postgres@localhost/dead-or-alive-test-db"
 };
