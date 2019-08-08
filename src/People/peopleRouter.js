cb0c77df7628fa9644ea88ea5b06735ee8a9c254
Ben Cohen <benjcohen1985@gmail.com>
2019-08-08 16:57:44 -0400

    Add post person endpoint

--- src/People/peopleRouter.js
+++ src/People/peopleRouter.js
@@ -11,6 +11,26 @@ peopleRouter.route("/").get((req, res, next) => {
       res.json(people);
     })
     .catch(next);
+
+  peopleRouter.route("/").post(jsonParser, (req, res, next) => {
+    const knexInstance = req.app.get("db");
+    const { person_name, dates, status, description } = req.body;
+    const newPerson = { person_name, dates, status, description };
+    for (const [key, value] of Object.entries(newPerson))
+      if (value === null) {
+        return res
+          .status(400)
+          .json({ error: { message: `Missing ${key} in request body` } });
+      }
+
+    peopleServices
+      .postPerson(knexInstance, newPerson)
+      .then(person => {
+        const newPerson = person[0];
+        res.status(201).json(newPerson);
+      })
+      .catch(next);
+  });
 });
 
 module.exports = peopleRouter;
