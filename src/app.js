a8e72845e76e75fcb017379376c0f6f0f01ca34d
Ben Cohen <benjcohen1985@gmail.com>
2019-08-08 14:23:30 -0400

    Change logging format

--- src/app.js
+++ src/app.js
@@ -7,8 +7,7 @@ const { NODE_ENV } = require("./config");
 
 const app = express();
 
-const morganOption = NODE_ENV === "production";
-app.use(morgan(morganOption));
+app.use(morgan("combined"));
 app.use(cors());
 app.use(helmet());
 
