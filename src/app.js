5241a5032b22c91e34fd6757d28694bde306760c
Ben Cohen <benjcohen1985@gmail.com>
2019-08-20 10:40:37 -0400

    Add middleware for 'not_in_db' table

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 const peopleRouter = require("./People/peopleRouter");
+const unknownPersonRouter = require("./People/unknownPersonRouter");
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(helmet());
 app.use("/api/people", peopleRouter);
+app.use("/api/not_in_db", unknownPersonRouter);
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
