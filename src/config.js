58292fff1a9f8b38f0f78135234a2d4dd3254ca5
Ben Cohen <benjcohen1985@gmail.com>
2019-08-21 10:28:30 -0400

    Update production env variable

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DB_URL: "postgresql://postgres@localhost/dead-or-alive",
-  DATABASE_URL: "postgresql://postgres@localhost/dead-or-alive",
+  DATABASE_URL: process.env.DATABASE_URL,
   TEST_DB_URL: "postgresql://postgres@localhost/dead-or-alive-test-db"
 };
