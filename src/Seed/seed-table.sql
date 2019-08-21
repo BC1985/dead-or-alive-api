5eb7957ebbec9a22ed635eac79f28fdbe9eeb473
Ben Cohen <benjcohen1985@gmail.com>
2019-08-21 10:29:06 -0400

    Insert data

--- src/Seed/seed-table.sql
+++ src/Seed/seed-table.sql
@@ -2,7 +2,7 @@
 INSERT INTO people(person_name,dates,status,description, image) 
 VALUES ('Ray Manzarek', '', '','', '','https://upload.wikimedia.org/wikipedia/commons/4/4c/Ray_Manzarek.JPG'),
 VALUES ('Duane Allman', '', '','', '','https://upload.wikimedia.org/wikipedia/commons/6/68/Duane_Allman.jpg'),
-VALUES ('Kurt Cobain', '', '','', '','https://upload.wikimedia.org/wikipedia/commons/5/59/Kurt_St_Thomas_1991_cropped.jpg'),
+VALUES ('Kurt Cobain', 'February 20, 1967 - April 5, 1994', 'dead','Starting to smell funny','https://upload.wikimedia.org/wikipedia/commons/5/59/Kurt_St_Thomas_1991_cropped.jpg');
 VALUES ('Gregg Allman', '', '','', '','https://upload.wikimedia.org/wikipedia/commons/6/6f/Gregg_Allman_1975.JPG'),
 VALUES ('Jim Morrison', '', '','', '','https://upload.wikimedia.org/wikipedia/commons/7/7f/Jim_Morrison_1969.JPG'),
 VALUES ('Janis Joplin', '', '','', '','https://upload.wikimedia.org/wikipedia/commons/7/79/Janis_Joplin_in_1969_%28cropped%29.png'),
