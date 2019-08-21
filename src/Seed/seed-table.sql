2a0c66ec3e91f83869aff11b34317cfa0800ff78
Ben Cohen <benjcohen1985@gmail.com>
2019-08-21 07:55:19 -0400

    Add info

--- src/Seed/seed-table.sql
+++ src/Seed/seed-table.sql
@@ -1,5 +1,10 @@
-INSERT INTO people(person_name,dates,status,description) 
-VALUES ('George Carlin', 'May 12, 1937-June 22, 2008','Dead', 'Got his five minute warning'),
-('Zubin Mehta', 'Born April 29, 1936', 'Alive','Alive and smelling the roses'),
-('Andrew "Dice" Clay', 'Born September 29, 1957', 'Alive','Still among the living'),
-('Ludwig Van Beethoven', '1770-1827','Dead', 'Expired and joined the choir invisible');
+
+INSERT INTO people(person_name,dates,status,description, image) 
+VALUES ('Ray Manzarek', '', '','', '','https://upload.wikimedia.org/wikipedia/commons/4/4c/Ray_Manzarek.JPG'),
+VALUES ('Duane Allman', '', '','', '','https://upload.wikimedia.org/wikipedia/commons/6/68/Duane_Allman.jpg'),
+VALUES ('Kurt Cobain', '', '','', '','https://upload.wikimedia.org/wikipedia/commons/5/59/Kurt_St_Thomas_1991_cropped.jpg'),
+VALUES ('Gregg Allman', '', '','', '','https://upload.wikimedia.org/wikipedia/commons/6/6f/Gregg_Allman_1975.JPG'),
+VALUES ('Jim Morrison', '', '','', '','https://upload.wikimedia.org/wikipedia/commons/7/7f/Jim_Morrison_1969.JPG'),
+VALUES ('Janis Joplin', '', '','', '','https://upload.wikimedia.org/wikipedia/commons/7/79/Janis_Joplin_in_1969_%28cropped%29.png'),
+VALUES ('Leonard Cohen', '', '','', '','https://upload.wikimedia.org/wikipedia/commons/b/b4/Leonard_Cohen%2C_1988_01.jpg'),
+VALUES ('Joe Strummer', '', '','', '','https://upload.wikimedia.org/wikipedia/commons/7/73/Joe_Strummer_performing_at_St_Ann%27s_Warehouse%2C_Brooklyn_-_NYC_Apr_5_2002.jpg'),
