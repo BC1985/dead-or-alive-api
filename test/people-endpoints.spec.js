a3c5b3215c8931a83252297ccbd2a79565d32798
Ben Cohen <benjcohen1985@gmail.com>
2019-08-22 10:23:12 -0400

    Import test functions and add tests

--- test/people-endpoints.spec.js
+++ test/people-endpoints.spec.js
@@ -2,6 +2,7 @@ const knex = require("knex");
 const app = require("../src/app");
 const { TEST_DB_URL } = require("../src/config");
 
+const helpers = require("./test-helpers");
 describe("people endpoints", () => {
   let db;
   before("make knex instane", done => {
@@ -11,6 +12,7 @@ describe("people endpoints", () => {
     });
     app.set("db", db);
     done();
+    helpers.cleanTables();
   });
   after("disconnect from db", () => db.destroy());
   describe("GET api/people", () => {
@@ -22,5 +24,42 @@ describe("people endpoints", () => {
           .expect(200, []);
       });
     });
+    context("given there are people in the database", () => {
+      const peopleArray = helpers.makePeopleArray();
+      beforeEach("insert people", () => {
+        return db.into("people").insert(peopleArray);
+      });
+      afterEach("clean tables", () => {
+        helpers.cleanTables();
+      });
+      it("responds with 200 and array of people", () => {
+        return supertest(app)
+          .get("/api/people")
+          .expect(200, peopleArray);
+      });
+    });
+    // describe("GET /api/people/:id", () => {
+    //   context("given no people in db", () => {
+    //     it("responds with 404", () => {
+    //       const personId = 12345;
+    //       return supertest(app)
+    //         .get(`/api/people/${personId}`)
+    //         .expect(404, { error: { message: "Person does not exist" } });
+    //     });
+    //   });
+    //   context("Given there are people in the database", () => {
+    //     const peopleArray = helpers.makePeopleArray();
+    //     beforeEach("insert people", () => {
+    //       return db.into("people").insert(peopleArray);
+    //     });
+    //     it("responds with 200 the specified person", () => {
+    //       const personId = 2;
+    //       const expectedPerson = peopleArray[personId - 1];
+    //       supertest(app)
+    //         .get(`/api/reviews/${personId}`)
+    //         .expect(200, expectedPerson);
+    //     });
+    //   });
+    // });
   });
 });
