605f7a40f2f6f1301a99ffe9624516e32115cfda
Ben Cohen <benjcohen1985@gmail.com>
2019-08-23 23:12:06 -0400

    Add local URL

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DB_URL: process.env.DATABASE_URL,
-  TEST_DB_URL: "postgresql://postgres@localhost/dead-or-alive-test-db"
+  TEST_DB_URL: "postgresql://postgres@localhost/dead-or-alive-test-db",
+  LOCAL_URL: "postgresql://postgres@localhost/dead-or-alive"
 };
