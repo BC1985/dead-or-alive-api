65ca24f9c3460e9e33e1a06a60e138bbf77583e9
Ben Cohen <benjcohen1985@gmail.com>
2019-08-24 21:27:18 -0400

    Calculate person age based on DOB

---
+++ src/Utils/age.js
@@ -0,0 +1,13 @@
+function getAge(DOB) {
+  var today = new Date();
+  var birthDate = new Date(DOB);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age = age - 1;
+  }
+
+  console.log(age);
+}
+
+getAge("2/4/1948");
